ca7c280fe2a5d75b417029255e22944878f0cfb4
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-23 20:02:07 -0500

    Adaptando el componente Contador.js

--- src/componentes/Contador.js
+++ src/componentes/Contador.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../hojas-de-estilo/Contador.css'
 
-function Contador({ numeroClics }) {
-  return (
-    <div className='contador'>
-      { numeroClics }
-    </div>
-  );
+class Contador extends React.Component {
+  render() {
+    return (
+      <div className='contador'>
+        { this.props.numeroClics }
+      </div>
+    );
+  };
 }
 
 export default Contador;
