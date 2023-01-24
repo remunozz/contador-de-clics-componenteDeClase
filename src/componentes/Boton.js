e7e45de2d031a80805182b47f971aded1660bdda
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-23 20:20:36 -0500

    Adaptando el componente Boton.js

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../hojas-de-estilo/Boton.css';
 
-function Boton({ texto, esBotonDeClic, manejarClic}){
-  return(
-    <button
-      className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
-      onClick={manejarClic}>
-      { texto }
-    </button>
-  );
+class Boton extends React.Component {
+    render() { 
+      return (
+        <button
+          className={this.props.esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+          onClick={this.props.manejarClic} >
+          {this.props.texto}
+        </button>
+      );
+    };
 }
 
 export default Boton;
